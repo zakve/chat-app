293d9fd27e3c6925dcab0c1c300084eefda5bb62
zakve <zaklas.martin@gmail.com>
2022-04-30 15:53:19 +0200

    Add send location event

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,3 +10,12 @@ document.querySelector('#message-form').addEventListener('submit', e => {
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation)
+        return alert('Geolocation is not supported by your browser.')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+    })
+})
