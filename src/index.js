ef4921a8011b92e8aa0cb8e983cf07bfdadf98f5
zakve <zaklas.martin@gmail.com>
2022-04-30 11:37:07 +0200

    Add send, broadcast and listen messages

--- src/index.js
+++ src/index.js
@@ -13,8 +13,15 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New web socket connection')
+
+    socket.emit('message', "Welcome!")
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', message => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(port, () => {
