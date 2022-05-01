e8f2bb557e9ce660a988b6ca599dd7bb80fc3ef7
zakve <zaklas.martin@gmail.com>
2022-05-01 18:15:10 +0200

    Add timestamps for messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,15 +14,17 @@ const locationTemplate = document.querySelector('#location-template').innerHTML
 socket.on('message', message => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
-        message: message
+        message: message.text,
+        createdAt: moment(message.createdAt).format("H:mm")
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', url => {
-    console.log(url)
+socket.on('locationMessage', message => {
+    console.log(message)
     const html = Mustache.render(locationTemplate, {
-        url
+        url: message.url,
+        createdAt: moment(message.createdAt).format("H:mm")
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
