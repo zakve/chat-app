df5b1dc0a0df7c7b80191041c1ce7a04e9ba5e89
zakve <zaklas.martin@gmail.com>
2022-05-14 09:14:27 +0200

    Add list of users in room in sidebar

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ io.on('connection', (socket) => {
 
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
@@ -55,10 +59,13 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
-        console.log(user)
 
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 })
